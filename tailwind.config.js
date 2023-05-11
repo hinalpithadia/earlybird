72365c2cf38d3545ee5df74f4bb7959adb31f649
hinalpithadia <38879377+hinalpithadia@users.noreply.github.com>
2023-05-11 15:06:55 +0530

    earlybird page created

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  
+  content: [
+    './src/**/*.{html,js}',    
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+        inter: ['Inter', 'serif'],
+      },
+    },
+  },
+  plugins: [
+    
+  ],
+ 
+}
+
